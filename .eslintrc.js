2ccefac28290d25ad47b3b4e4e600aa68925ef97
Matt Chapman <chapabu101@gmail.com>
2019-12-02 12:53:10 +0000

    Ignore import/no-unresolved for Fractal peerdep

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
   rules: {
     'no-underscore-dangle': ['error', { allowAfterThis: true }],
-    'no-param-reassign': 0
+    'no-param-reassign': 0,
+    'import/no-unresolved': [2, { ignore: ['@frctl/fractal'] }]
   }
 };
