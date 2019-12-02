bef78c0d7c6b44ffe2cd70ec6e19a03d837e3b4d
Matt Chapman <chapabu101@gmail.com>
2019-12-02 12:35:48 +0000

    Unset some "Fractal enforced" rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ['airbnb-base', 'plugin:prettier/recommended']
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  rules: {
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'no-param-reassign': 0
+  }
 };
